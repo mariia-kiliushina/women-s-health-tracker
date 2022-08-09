d58b025e81e530705e2f2b106d7f41dcc5962658
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-09 19:24:26 +0400

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react'
-
 // import { Provider } from 'react-redux'
-// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import Input from '#components/Input'
 
 const App: FC = () => {
   return (
     // <Provider store={store}>
-    //   <Router>
-    <div className="main"></div>
-    //   </Router>
+    <Router>
+      <div className="main">
+        <Route path="/" element={Input} />
+      </div>
+    </Router>
     // </Provider>
   )
 }
