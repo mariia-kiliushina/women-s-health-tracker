2de6375cd37e5d7b3a9f39ef2f1567c3494e91e3
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-10 14:53:00 +0400

    add favicon loader

--- express.js
+++ express.js
@@ -3,6 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 8081
 const periods = require('./periodsData')
 
+const favicon = require('express-favicon')
+express.use(favicon(__dirname + '/public/flower.png'))
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
