56372c91611d3c9d608b23a17bf46696cade82a4
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-10 15:20:47 +0400

    somecommit

--- express.js
+++ express.js
@@ -3,13 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 8081
 const periods = require('./periodsData')
 
-const favicon = require('express-favicon')
-express.use(favicon(__dirname + '/public/flower.png'))
+// const favicon = require('express-favicon')
+// express.use(favicon(__dirname + '/public/flower.png'))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .get('/api/periods', (req, res) => res.send(periods))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
