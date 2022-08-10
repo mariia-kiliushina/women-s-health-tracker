37b1b409a703105cc5d0a1e13e861fd9805ac302
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-08-10 18:35:35 +0400

    version with redux and getting data partially from backend

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,10 +4,21 @@ import { useDispatch } from 'react-redux'
 import { addTrack } from '../../store/sliceData'
 
 const Input: FC = () => {
+  const getTodatDateString = () => {
+    const currentDate = new Date().toJSON().slice(0, 10)
+    return currentDate
+  }
   const dispatch = useDispatch()
-  const [state, setState] = useState({ date: '', type: '' })
+  const [error, setError] = useState()
+  const [state, setState] = useState({ date: getTodatDateString(), type: '' })
 
-  const onAdd = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    if (!state.type) {
+      //@ts-ignore
+      setError('Choose type')
+      return
+    }
     dispatch(addTrack(state))
   }
   const onChange = (event: any) => {
@@ -23,15 +34,21 @@ const Input: FC = () => {
 
   return (
     <>
-      <label>Input date</label>
-      <input name="date" type="date" onChange={onChange} />
-      <select name="type" onChange={onChange}>
-        <option value="Had flows">Had flows</option>
-        <option value="No flows">No flows</option>
-        <option value="Breast pain">Breast pain</option>
-        <option value="Meds">Meds</option>
-      </select>
-      <button onClick={onAdd}>Add data</button>
+      <form onSubmit={handleSubmit}>
+        <label>Input date</label>
+        <input name="date" type="date" onChange={onChange} value={state.date} />
+        <select name="type" defaultValue="choose" onChange={onChange}>
+          <option value="choose" hidden>
+            Choose event type
+          </option>
+          <option value="Had flows">Had flows</option>
+          <option value="No flows">No flows</option>
+          <option value="Breast pain">Breast pain</option>
+          <option value="Meds">Meds</option>
+        </select>
+        <button type="submit">Add data</button>
+        {error && <div>Choose type</div>}
+      </form>
     </>
   )
 }
